ee9898147a361e4850323784b5694bea4b2b0d20
anoushayzeaghum <anoushayzeaghum2001@gmail.com>
2026-03-05 12:32:47 +0500

    Serializer created for Mouza, Murabba and Khasra

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -5,10 +5,6 @@ from .models import *
 # --------------------------------------------------------
 # Mouza Serializer
 # --------------------------------------------------------
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
-from .models import Mouza
-
-
 class MouzaSerializer(GeoFeatureModelSerializer):
 
     class Meta:
@@ -31,11 +27,68 @@ class MouzaSerializer(GeoFeatureModelSerializer):
             "geom",
         )
         
-    
 # --------------------------------------------------------
 # Murabba Serializer
 # --------------------------------------------------------
+class MurabbaSerializer(GeoFeatureModelSerializer):
 
+    class Meta:
+        model = Murabba
+        geo_field = "geom"
+        id_field = "id"
+
+        fields = (
+            "id",
+            "district",
+            "dist_id",
+            "tehsil",
+            "tehsil_id",
+            "qh",
+            "qh_id",
+            "pc",
+            "pc_id",
+            "mouza",
+            "mouza_id",
+            "murabba_no",
+            "sheets",
+            "geom",
+        )
 # --------------------------------------------------------
 # Khasra Serializer
 # --------------------------------------------------------
+class KhasraSerializer(GeoFeatureModelSerializer):
+
+    class Meta:
+        model = Khasra
+        geo_field = "geom"
+        id_field = "id"
+
+        fields = (
+            "id",
+            "join_shp",
+            "district",
+            "dist_id",
+            "tehsil",
+            "tehsil_id",
+            "qh",
+            "qh_id",
+            "pc",
+            "pc_id",
+            "mouza",
+            "mouza_id",
+            "type",
+            "m",
+            "a",
+            "k",
+            "sk",
+            "label",
+            "karam",
+            "remarks",
+            "khasra_id",
+            "b",
+            "mn",
+            "division",
+            "khewat_id",
+            "divn_id",
+            "geom",
+        )
