d354bc8d2ce6a88cff39c1a4dbf5425055ac1163
sana-ali18 <sana.znz18@gmail.com>
2026-06-17 15:23:22 +0500

    add khasra id and murabba id

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -690,6 +690,8 @@ class WSPointSerializer(GeoFeatureModelSerializer):
 class ProjectMauzaSerializer(serializers.ModelSerializer):
 
     mauza_detail = MauzaSerializer(source="mauza", read_only=True)
+    khasra_detail = KhasraSerializer(source="khasra", read_only=True)
+    murabba_detail = MurabbaSerializer(source="murabba", read_only=True)
 
     class Meta:
         model = ProjectMauza
@@ -697,7 +699,7 @@ class ProjectMauzaSerializer(serializers.ModelSerializer):
             "id",
             "project",
             "mauza",
-            "mauza_detail",
+            "mauza_detail", "khasra", "khasra_detail", "murabba", "murabba_detail",
         )
 
     
