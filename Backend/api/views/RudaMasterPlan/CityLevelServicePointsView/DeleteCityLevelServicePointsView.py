16fa78215272bfb064be3701aa73b3ed52b9807d
anoushayzeaghum <anoushayzeaghum2001@gmail.com>
2026-07-10 13:02:59 +0500

    CityLevelServicePoints

---
+++ Backend/api/views/RudaMasterPlan/CityLevelServicePointsView/DeleteCityLevelServicePointsView.py
@@ -0,0 +1,42 @@
+from ...common_imports import *
+
+
+class DeleteCityLevelServicePointsView(viewsets.ViewSet):
+    queryset = CityLevelServicePoints.objects.all()
+    serializer_class = CityLevelServicePointsSerializer
+    permission_classes = [AllowAny]
+
+    def destroy(self, request, *args, **kwargs):
+        record_id = kwargs.get("pk")
+
+        try:
+            record = CityLevelServicePoints.objects.get(gid=record_id)
+        except CityLevelServicePoints.DoesNotExist:
+            return ApiResponse(
+                status=status.HTTP_404_NOT_FOUND,
+                message="CityLevelServicePoints not found.",
+                http_status=status.HTTP_404_NOT_FOUND,
+            ).create_response()
+
+        try:
+            record.delete()
+
+            return ApiResponse(
+                status=status.HTTP_200_OK,
+                message="CityLevelServicePoints deleted successfully.",
+                http_status=status.HTTP_200_OK,
+            ).create_response()
+
+        except ProtectedError:
+            return ApiResponse(
+                status=status.HTTP_400_BAD_REQUEST,
+                message="Cannot delete this CityLevelServicePoints because it is linked to other records.",
+                http_status=status.HTTP_400_BAD_REQUEST,
+            ).create_response()
+
+        except Exception as e:
+            return ApiResponse(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                message=str(e),
+                http_status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            ).create_response()
