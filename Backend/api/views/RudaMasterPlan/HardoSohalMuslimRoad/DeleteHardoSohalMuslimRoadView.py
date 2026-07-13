bdef1767a484e74044b028c2769c7092e63cd84e
anoushayzeaghum <anoushayzeaghum2001@gmail.com>
2026-07-13 12:31:35 +0500

    HardoSohalMuslimRoad

---
+++ Backend/api/views/RudaMasterPlan/HardoSohalMuslimRoad/DeleteHardoSohalMuslimRoadView.py
@@ -0,0 +1,42 @@
+from ...common_imports import *
+
+
+class DeleteHardoSohalMuslimRoadView(viewsets.ViewSet):
+    queryset = HardoSohalMuslimRoad.objects.all()
+    serializer_class = HardoSohalMuslimRoadSerializer
+    permission_classes = [AllowAny]
+
+    def destroy(self, request, *args, **kwargs):
+        record_id = kwargs.get("pk")
+
+        try:
+            record = HardoSohalMuslimRoad.objects.get(gid=record_id)
+        except HardoSohalMuslimRoad.DoesNotExist:
+            return ApiResponse(
+                status=status.HTTP_404_NOT_FOUND,
+                message="HardoSohalMuslimRoad not found.",
+                http_status=status.HTTP_404_NOT_FOUND,
+            ).create_response()
+
+        try:
+            record.delete()
+
+            return ApiResponse(
+                status=status.HTTP_200_OK,
+                message="HardoSohalMuslimRoad deleted successfully.",
+                http_status=status.HTTP_200_OK,
+            ).create_response()
+
+        except ProtectedError:
+            return ApiResponse(
+                status=status.HTTP_400_BAD_REQUEST,
+                message="Cannot delete this HardoSohalMuslimRoad because it is linked to other records.",
+                http_status=status.HTTP_400_BAD_REQUEST,
+            ).create_response()
+
+        except Exception as e:
+            return ApiResponse(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                message=str(e),
+                http_status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            ).create_response()
