ccc7b49a1d318cc8446bd44531311f511f4142d6
sana-ali18 <sana.znz18@gmail.com>
2026-07-28 16:15:29 +0500

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -203,8 +204,10 @@ export const getKhasras = async (mauza_id) => {
   const res = await API.get("/khasra/", {
     params: { mauza_id },
   });
+  console.log(res);
   return normalizeGeoJson(res);
 };
+
 
 export const importKhasra = async ({ file }) => {
   const formData = new FormData();
