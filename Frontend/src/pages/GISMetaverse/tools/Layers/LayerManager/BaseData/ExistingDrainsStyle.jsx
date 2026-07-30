0b727c3899260946bec0eb3de2b33f53d25ff2a3
anoushayzeaghum <anoushayzeaghum2001@gmail.com>
2026-07-30 16:11:56 +0500

    Existing Drain Frontend and Backend Implemented

---
+++ Frontend/src/pages/GISMetaverse/tools/Layers/LayerManager/BaseData/ExistingDrainsStyle.jsx
@@ -0,0 +1,12 @@
+const ExistingDrainsStyle = Object.freeze({
+  color: "#38bdf8",
+  fillOpacity: 0.2,
+  lineOpacity: 1,
+  lineWidth: ["interpolate", ["linear"], ["zoom"], 7, 1.5, 14, 3.5],
+  lineDasharray: [2, 1],
+  pointRadius: ["interpolate", ["linear"], ["zoom"], 7, 3, 15, 6],
+  pointStrokeColor: "#ffffff",
+  pointStrokeWidth: 1,
+});
+
+export default ExistingDrainsStyle;
